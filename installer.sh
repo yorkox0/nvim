09495e9570dec4a207a4a6915aa54abc3083e7bc
yorkox <49209840+yorkox0@users.noreply.github.com>
2021-11-04 22:41:56 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,49 @@
+green="\e[0;32m\033[1m"
+end="\033[0m\e[0m"
+red="\e[0;31m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+purple="\e[0;35m\033[1m"
+turquoise="\e[0;36m\033[1m"
+grayr="\e[0;37m\033[1m"
 
+function main(){
 
-pip install neovim
-sudo npm i -g neovim
+       echo -e "${red}[!] ${yellow}Installing nvim..."
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       sleep 2
+       
+       echo
+       pip install neovim 2>/dev/null
+       sudo npm i -g neovim 2>/dev/null
+
+       echo -e "${red}[!] ${yellow}Installing vim-plug..."
+
+       sleep 2
+       
+       echo
+       sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+              https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
       
-mkdir ~/.config/nvim/ 2>/dev/null
+       echo -e "${red}[!] ${yellow}Clonning config"
+      
+       sleep 2
+      
+       echo
+       mkdir ~/.config/nvim/ 2>/dev/null
 
-cp -r nvim ~/.config/
+       cp -r nvim ~/.config/ 2>/dev/null
+       
+       echo -e "${green}[+] Done!"
+       
+       echo
+       sleep 1
+
+       echo -e "${red}[!] ${yellow}Executte 'nvim' and run :PlugInstall"
+       
+       echo
+       sleep 2
+
+}
+
+main
